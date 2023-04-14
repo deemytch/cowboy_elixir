4613c7a5aa36b9f07ded0330bf9fd4984eb7de4f
Dimytch <dimitri@pekarovsky.name>
2023-04-14 18:40:02 +0200

    het begon

---
+++ lib/yaml_elixir.ex
@@ -0,0 +1,18 @@
+defmodule YamlElixir do
+  @moduledoc """
+  Documentation for `YamlElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> YamlElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/yaml_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule YamlElixirTest do
+  use ExUnit.Case
+  doctest YamlElixir
+
+  test "greets the world" do
+    assert YamlElixir.hello() == :world
+  end
+end
